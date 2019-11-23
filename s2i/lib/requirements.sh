cf501b4f2b73022bc15ce6b3313189228bd7d92b
Marek Cermak <macermak@redhat.com>
2019-11-23 12:32:31 +0100

    new: dev: Added notebook requirements extraction
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    new file:   s2i/lib/main
    new file:   s2i/lib/requirements.sh

---
+++ s2i/lib/requirements.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# TODO: This lib can eventually be turned into a Python script
+
+set -euxo
+
+
+die() {
+	echo -e "$1" &>2
+	exit 1
+}
+
+_cleanup() {
+	return 0
+}
+
+# Get notebook requirements
+#
+# args:
+#   $1: path to the notebook
+# yields: space-separated list of <package><constraint>
+notebook::requirements::get() {
+	local notebook="$1"
+
+	local requirements=$(
+		cat "$notebook" | \
+		jq -r '.metadata.requirements | { "dev-packages" } * { packages } | add | to_entries' |\
+		jq -c '.[]' \
+	)
+
+	local packages=""
+	for r in ${requirements}; do
+		package=$(echo "$r" | jq -r '.key')
+		version=$(echo "$r" | jq -r '.value')
+
+		if [ "$version" == '*' ]; then
+			packages=$(echo "$packages" "$package")
+		else
+			packages=$(echo "$packages" "$package$version")
+		fi
+	done
+
+	echo ${packages}; return 0
+}
+
+# Install notebook requirements
+#
+# env:
+#   [required] JUPYTER_NOTEBOOK_PATH: path to the notebook
+notebook::requirements::install() {
+	: "${JUPYTER_NOTEBOOK_PATH?Must set JUPYTER_NOTEBOOK_PATH environment variable}"
+
+	[ ! -f "$JUPYTER_NOTEBOOK_PATH" ] && die "File $JUPYTER_NOTEBOOK_PATH not found."
+
+	echo "--- Installing notebook requirements"
+
+	local requirements=$(notebook::requirements::get "$JUPYTER_NOTEBOOK_PATH")
+	if [ -z ${requirements} ]; then
+		&>2 echo -e "Notebook doesn't have any requirements. Skipping installation."
+		return 0
+	else
+		echo -e "Required packages: ${requirements}"""
+		pipenv run pip install ${requirements}
+	fi
+
+	_cleanup
+}
