9a4d6434221b0f1e2ebe7e5684c8dd55a0110d2a
Marek Cermak <macermak@redhat.com>
2019-11-24 12:18:59 +0100

    chg: dev: Output models to /mnt instead of /tmp
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   ../../../s2i/lib/common.sh

--- s2i/lib/common.sh
+++ s2i/lib/common.sh
@@ -6,7 +6,7 @@ set -euxo
 export JUPYTER_NOTEBOOK_KERNEL="${JUPYTER_NOTEBOOK_KERNEL:-python3}"
 
 # Output directory for models, checkpoints, etc...
-export MODEL_OUTPUT_DIR="/tmp/models"
+export MODEL_OUTPUT_DIR="/mnt/artifacts/models"
 
 die() {
 	>&2 echo -e "$1"
