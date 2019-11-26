e9cd72b094019c77d3becd930e3299946e7a8c28
Marek Cermak <macermak@redhat.com>
2019-11-26 15:26:19 +0100

    new: dev: Added tests to the execution module
    Signed-off-by: Marek Cermak <macermak@redhat.com>
    
    modified:   s2i/lib/execution_test.sh

--- s2i/lib/execution_test.sh
+++ s2i/lib/execution_test.sh
@@ -16,6 +16,8 @@ export JUPYTER_NOTEBOOK_PATH="${REPO_ROOT}/test/s2i/test-app/test-notebook.ipynb
 
 
 test::notebook::execution::run() {
+    echo "---> Testing notebook::execution::run"
+
     local tmpdir="$(mktemp -td 'jupyter-notebook-s2i-test-XXX')"
 
     export PAPERMILL_OUTPUT_PATH="${tmpdir}/output.ipynb"
@@ -23,8 +25,16 @@ test::notebook::execution::run() {
 
     notebook::execution::run
 
-    # check that the directory contains the output notebook
-    rm -rf "${tmpdir}"
+    [ ! -f "$PAPERMILL_OUTPUT_PATH" ] \
+        && die "Missing expected output notebook."
+    [ "$(cat "$PAPERMILL_OUTPUT_PATH")" == "" ] \
+        && die "Empty output notebook."
+    [ "$(cat "$PAPERMILL_OUTPUT_PATH" | jq -c '.metadata.papermill')" == "" ] \
+        && die "Missing Papermill metadata."
+    [ "$(cat "$PAPERMILL_OUTPUT_PATH" | jq -c '.metadata.papermill.exception')" != "null" ] \
+        && die "Unexpected exception occurred."
+
+    echo "---> Success."
 }
 
 if [ "$0" = "$BASH_SOURCE" ] ; then
